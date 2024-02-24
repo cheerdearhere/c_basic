52b695943d078cb273090dd203ca9473bca2005e
dreamik <dream-ik89@naver.com>
2024-02-24 16:57:52 +0900

    build: function project

--- c_basic/ch13Function/function_basic.c
+++ c_basic/ch13Function/function_basic.c
@@ -2,24 +2,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void startChapter(char chapterName[]) {
+/* only basic */  
+void StartChapter(char chapterName[]) {
 	printf("%s 연습 시작\n", chapterName, _countof(chapterName));
 }
-void closeChapter(char chapterName[]) {
+void CloseChapter(char chapterName[]) {
 	printf("%s 연습 종료\n", chapterName, _countof(chapterName));
 }
+/* // only basic */
 
-int Add(int a, int b){
-	int nData = 0;
-	nData = a + b;
-	return nData;
-}
 
 int main(void) {
-	startChapter("함수");
-
+	StartChapter("함수 ");
 	
-
-	closeChapter("함수");
+	
+	CloseChapter("함수");
 	return 0;
 }
