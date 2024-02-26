1781c92ad4a2a75d70b40b923c436f6f86637319
dreamik <dream-ik89@naver.com>
2024-02-26 23:48:32 +0900

    chore: pointer_basic add library

--- c_basic/ch14Pointer/pointer_basic.c
+++ c_basic/ch14Pointer/pointer_basic.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // malloc()/ _countof()
+#include <string.h> // memset memcmp memcpy
 #include <stdbool.h>
 #include "basicform.h"
 
 int main(void) {
 	StartChapter();
-	
+
 
 
 	CloseChapter();
