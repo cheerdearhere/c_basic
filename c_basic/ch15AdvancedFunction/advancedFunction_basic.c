3a17a4a5d4568b98df1d72337b94a50e1be7a6f6
dreamik <dream-ik89@naver.com>
2024-02-28 16:59:13 +0900

    build: AdvancedFunction 실습 준비

--- c_basic/ch15AdvancedFunction/advancedFunction_basic.c
+++ c_basic/ch15AdvancedFunction/advancedFunction_basic.c
@@ -4,15 +4,11 @@
 #include <stdbool.h>
 #include "basicform.h"
 
-//int TestFunc(void); 
-int main(int argc, char* argv[]) {
-	StartChapter();
 
+int main(int argc, char* argv[]) {
+	StartChapter(); 
 
 
 	CloseChapter();
 	return 0;
 }
-//int TestFunc(void) {
-//
-//}
