514441a68da673c050e5891c71858fb944e9eaf8
dreamik <dream-ik89@naver.com>
2024-02-28 12:56:34 +0900

    build: 함수 응용편 실습 준비

---
+++ ch15AdvancedFunction/advancedFunction_basic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <stdbool.h>
+#include "basicform.h"
+
+//int TestFunc(void); 
+int main(int argc, char* argv[]) {
+	StartChapter();
+
+
+
+	CloseChapter();
+	return 0;
+}
+//int TestFunc(void) {
+//
+//}
---
+++ ch15AdvancedFunction/basic_farmatter.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* only basic */
+void StartChapter(void) {
+	printf("함수 응용 연습 시작\n");
+}
+void CloseChapter(void) {
+	printf("함수 응용 연습 종료\n");
+}
+/* // only basic */
