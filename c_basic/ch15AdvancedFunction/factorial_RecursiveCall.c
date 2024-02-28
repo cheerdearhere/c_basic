b8e902ea2c515cf9449393f878563d2a631aff9a
dreamik <dream-ik89@naver.com>
2024-02-28 17:07:23 +0900

    feat: make factorial function using recursive call

---
+++ c_basic/ch15AdvancedFunction/factorial_RecursiveCall.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int GetFactorial(int nParam) {
+	int nResult = 0;
+	if (nParam == 1) return 1;
+	nResult = nParam * GetFactorial(nParam - 1);
+	return nResult;
+}
+
+int main(int argc, char* argv[]) {
+
+	printf("5! = %d\n", GetFactorial(5));
+
+	return 0;
+}
