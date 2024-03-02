2f0eebcebddfc49f8ee93ddd68cfbed98839a52c
dreamik <dream-ik89@naver.com>
2024-03-02 14:36:35 +0900

    build: 시간 사용을 위한 헤더 추가

--- c_basic/ch15AdvancedFunction/advancedFunction_basic.c
+++ c_basic/ch15AdvancedFunction/advancedFunction_basic.c
@@ -2,12 +2,13 @@
 #include <stdlib.h> 
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
 #include "basicform.h"
 
 
 int main(int argc, char* argv[]) {
 	StartChapter(); 
-
+	
 
 	CloseChapter();
 	return 0;
