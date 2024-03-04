eb9b45f7350be941508e0a621bf579c0ef1ebdf6
dreamik <dream-ik89@naver.com>
2024-03-04 17:24:55 +0900

    build: preparing for File System

---
+++ c_basic/ch17FileSystem/basic_form.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* only basic */
+void StartChapter(void) {
+	printf("## 파일 시스템 시작 ##\n");
+}
+void CloseChapter(void) {
+	printf("## 파일 시스템 종료 ##\n");
+}
+/* // only basic */
---
+++ c_basic/ch17FileSystem/ch17FileSystem_basic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+
+#include <stdbool.h>
+#include <time.h>
+#include "basicform.h"
+
+
+int main(int argc, char* argv[]) {
+	StartChapter();
+
+
+	CloseChapter();
+	return 0;
+}
